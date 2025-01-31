e36dd4f115ef7f868b09c0280e5ec1bfc886544e
= <=>
2025-01-31 12:53:44 +0600

    Problem 16 optimization

--- src/medium/_016.rs
+++ src/medium/_016.rs
@@ -2,6 +2,40 @@ pub struct Solution;
 
 impl Solution {
     pub fn three_sum_closest(mut nums: Vec<i32>, target: i32) -> i32 {
+        nums.sort();
+
+        let n: usize = nums.len();
+        let mut closest_diff: i32 = i32::MAX;
+        let mut closest_sum: i32 = i32::MAX;
+
+        for i in 0..n - 2 {
+            let mut j: usize = i + 1;
+            let mut k: usize = n - 1;
+
+            while j < k {
+                let curr_sum: i32 = nums[i] + nums[j] + nums[k];
+
+                if curr_sum > target {
+                    k -= 1;
+                } else if curr_sum < target {
+                    j += 1;
+                } else {
+                    return target;
+                }
+
+                let curr_diff: i32 = (curr_sum - target).abs();
+
+                if closest_diff > curr_diff {
+                    closest_diff = curr_diff;
+                    closest_sum = curr_sum;
+                }
+            }
+        }
+
+        closest_sum
+    }
+
+    pub fn _three_sum_closest(mut nums: Vec<i32>, target: i32) -> i32 {
         nums.sort_unstable();
         let mut closest_sum: i32 = nums[0] + nums[1] + nums[2];
 
