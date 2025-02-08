4f56382d550f14fe0cdfb5e0d7bcb816370a19eb
= <=>
2025-02-08 08:48:17 +0600

    Problem 2349 and tests

---
+++ src/medium/_2349.rs
@@ -0,0 +1,84 @@
+// Design a number container system that can do the following:
+
+// Insert or Replace a number at the given index in the system.
+// Return the smallest index for the given number in the system.
+// Implement the NumberContainers class:
+
+// NumberContainers() Initializes the number container system.
+// void change(int index, int number) Fills the container at index with the number. If there is already a number at that index, replace it.
+// int find(int number) Returns the smallest index for the given number, or -1 if there is no index that is filled by number in the system.
+
+// Example 1:
+
+// Input
+// ["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"]
+// [[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]]
+// Output
+// [null, -1, null, null, null, null, 1, null, 2]
+
+// Explanation
+// NumberContainers nc = new NumberContainers();
+// nc.find(10); // There is no index that is filled with number 10. Therefore, we return -1.
+// nc.change(2, 10); // Your container at index 2 will be filled with number 10.
+// nc.change(1, 10); // Your container at index 1 will be filled with number 10.
+// nc.change(3, 10); // Your container at index 3 will be filled with number 10.
+// nc.change(5, 10); // Your container at index 5 will be filled with number 10.
+// nc.find(10); // Number 10 is at the indices 1, 2, 3, and 5. Since the smallest index that is filled with 10 is 1, we return 1.
+// nc.change(1, 20); // Your container at index 1 will be filled with number 20. Note that index 1 was filled with 10 and then replaced with 20.
+// nc.find(10); // Number 10 is at the indices 2, 3, and 5. The smallest index that is filled with 10 is 2. Therefore, we return 2.
+
+use std::collections::{BTreeSet, HashMap};
+
+pub struct NumberContainers {
+    data: HashMap<i32, BTreeSet<i32>>,
+    reverse: HashMap<i32, i32>,
+}
+
+impl NumberContainers {
+    pub fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+            reverse: HashMap::new(),
+        }
+    }
+
+    pub fn change(&mut self, index: i32, number: i32) {
+        if let Some(existing) = self.reverse.insert(index, number) {
+            if let Some(ordered_set) = self.data.get_mut(&existing) {
+                ordered_set.remove(&index);
+            }
+        }
+
+        self.data.entry(number).or_default().insert(index);
+    }
+
+    pub fn find(&self, number: i32) -> i32 {
+        self.data
+            .get(&number)
+            .and_then(|idxs: &BTreeSet<i32>| idxs.first().copied())
+            .unwrap_or(-1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_2349_1() {
+        let mut nc: NumberContainers = NumberContainers::new();
+
+        assert_eq!(-1, nc.find(10));
+
+        nc.change(2, 10);
+        nc.change(1, 10);
+        nc.change(3, 10);
+        nc.change(5, 10);
+
+        assert_eq!(1, nc.find(10));
+
+        nc.change(1, 20);
+
+        assert_eq!(2, nc.find(10));
+    }
+}
--- src/medium/mod.rs
+++ src/medium/mod.rs
@@ -51,6 +51,7 @@ pub mod _151;
 pub mod _162;
 pub mod _1726;
 pub mod _189;
+pub mod _2349;
 pub mod _3160;
 pub mod _3163;
 pub mod _3164;
