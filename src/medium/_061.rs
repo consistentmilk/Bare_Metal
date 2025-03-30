0e701231c950e2529befeb7e456ee1b5c6875e2e
= <=>
2025-03-30 08:54:17 -0400

    Problem 61 single solution optimization

--- src/medium/_061.rs
+++ src/medium/_061.rs
@@ -5,148 +5,40 @@ pub struct Solution;
 impl Solution {
     pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
         if head.is_none() {
-            return None;
+            return head;
         }
 
-        let k: usize = k as usize;
-        let mut ptr: &mut Option<Box<ListNode>> = &mut head;
-        let mut list: Vec<i32> = Vec::new();
+        let mut ptr: &Option<Box<ListNode>> = &head;
+        let mut n: i32 = 0;
 
         while let Some(node) = ptr {
-            list.push(node.val);
+            ptr = &node.next;
+            n += 1;
+        }
+
+        let k: i32 = k % n;
+
+        if k == 0 {
+            return head;
+        }
+
+        let mut i: i32 = 0;
+        let pivot_point: i32 = n - k;
+        let mut ptr: &mut Option<Box<ListNode>> = &mut head;
+        let mut pivot: Option<Box<ListNode>> = None;
+
+        while let Some(node) = ptr {
+            i += 1;
+
+            if i == pivot_point {
+                pivot = node.next.take();
+                break;
+            }
 
             ptr = &mut node.next;
         }
 
-        let mut head: Option<Box<ListNode>> = Some(Box::new(ListNode::new(0)));
-        let mut ptr: &mut Option<Box<ListNode>> = &mut head;
-        let n: usize = list.len();
-        let k: usize = n - (k % n);
-
-        if k == 0 {
-            for i in k..n {
-                let node: Option<Box<ListNode>> = Some(Box::new(ListNode::new(list[i])));
-                ptr.as_mut()?.next = node;
-                ptr = &mut ptr.as_mut()?.next;
-            }
-
-            return head?.next;
-        }
-
-        for i in k..n {
-            let node: Option<Box<ListNode>> = Some(Box::new(ListNode::new(list[i])));
-            ptr.as_mut()?.next = node;
-            ptr = &mut ptr.as_mut()?.next;
-        }
-
-        for i in 0..k {
-            let node: Option<Box<ListNode>> = Some(Box::new(ListNode::new(list[i])));
-            ptr.as_mut()?.next = node;
-            ptr = &mut ptr.as_mut()?.next;
-        }
-
-        head?.next
-    }
-}
-
-pub struct SolutionAltOne;
-
-impl SolutionAltOne {
-    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        // Edge cases: empty list or single-node list.
-        if head.is_none() || head.as_ref().unwrap().next.is_none() {
-            return head;
-        }
-
-        // First, compute the length of the list and get a mutable pointer to the tail.
-        let mut len: usize = 1;
-        {
-            let mut p: &mut Box<ListNode> = head.as_mut().unwrap();
-            while let Some(ref mut next_node) = p.next {
-                p = next_node;
-                len += 1;
-            }
-        }
-
-        // Compute the effective rotations needed.
-        let k: usize = (k as usize) % len;
-        if k == 0 {
-            return head;
-        }
-
-        // Now, traverse again to get the tail as a raw pointer.
-        let tail: *mut Box<ListNode> = {
-            let mut p: &mut Box<ListNode> = head.as_mut().unwrap();
-            while p.next.is_some() {
-                p = p.next.as_mut().unwrap();
-            }
-            p as *mut Box<ListNode>
-        };
-
-        // Form a circular list: set tail.next = head.
-        // Using unsafe to break the borrow so we can move head.
-        unsafe {
-            (*tail).next = head;
-        }
-
-        // To get the new tail, we need to move (len - k) steps from the beginning.
-        let steps_to_new_tail: usize = len - k;
-        // Start at the (old) head (which is now accessible via (*tail).next).
-        let mut new_tail: *mut Box<ListNode> =
-            unsafe { (*tail).next.as_mut().unwrap() as *mut Box<ListNode> };
-        for _ in 0..steps_to_new_tail - 1 {
-            unsafe {
-                new_tail = (*new_tail).next.as_mut().unwrap() as *mut Box<ListNode>;
-            }
-        }
-
-        // The new head is the node right after new_tail.
-        // Break the circle by setting new_tail.next to None.
-        unsafe {
-            let new_head: Option<Box<ListNode>> = (*new_tail).next.take();
-            new_head
-        }
-    }
-}
-
-pub struct SolutionAltTwo;
-
-impl SolutionAltTwo {
-    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        let mut ptr: Option<&Box<ListNode>> = head.as_ref();
-        let mut n: usize = 0;
-
-        while let Some(node) = ptr {
-            ptr = node.next.as_ref();
-            n += 1;
-        }
-
-        if n < 2 {
-            return head;
-        }
-
-        let k: usize = k as usize % n;
-
-        if k == 0 {
-            return head;
-        }
-
-        let mut i: usize = 0;
-        let mut ptr: Option<&mut Box<ListNode>> = head.as_mut();
-        let mut new_head: Option<Box<ListNode>> = None;
-
-        while let Some(node) = ptr {
-            if i + k == n - 1 {
-                new_head = node.next.take();
-                break;
-            } else {
-                ptr = node.next.as_mut();
-            }
-
-            i += 1;
-        }
-
-        let mut ptr: Option<&mut Box<ListNode>> = new_head.as_mut();
+        let mut ptr: &mut Option<Box<ListNode>> = &mut pivot;
 
         while let Some(node) = ptr {
             if node.next.is_none() {
@@ -154,10 +46,10 @@ impl SolutionAltTwo {
                 break;
             }
 
-            ptr = node.next.as_mut();
+            ptr = &mut node.next;
         }
 
-        new_head
+        pivot
     }
 }
 
